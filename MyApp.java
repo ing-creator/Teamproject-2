32d6f7a10d4bfaded0a9b290ea1fd021f94b55d7
yhj4 <yhj20040412@gmail.com>
2026-06-10 02:19:05 +0900

    Add files via upload

---
+++ MyApp.java
@@ -0,0 +1,20 @@
+
+/**
+ * MyApp 클래스
+ *
+ * @author (작성자 이름)
+ * @version (버전 번호 또는 작성한 날짜)
+ */
+public class MyApp
+{
+    public static void main(String[] args) {
+        POST post = new POST(); //POST객체 생성
+        
+        post.printProductList(); //매장 내 상품 목록들 출력
+        post.startSale(); //거래 시작
+        
+        post.scanBarcode(); //
+        post.finishSale(); //거래 종료
+        post.payment(); //결제
+    }
+}
