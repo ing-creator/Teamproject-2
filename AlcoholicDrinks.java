73d2ee30d25f3e5b01cbd7007b05723749792f5a
yhj4 <yhj20040412@gmail.com>
2026-06-11 21:40:58 +0900

    Update AlcoholicDrinks.java

--- AlcoholicDrinks.java
+++ AlcoholicDrinks.java
@@ -3,16 +3,19 @@
  * 주류에 대한 클래스
  *
  * @author (작성자 이름)
- * @version (버전 번호 또는 작성한 날짜)
+ * @version (2026.06.11)
  */
 public class AlcoholicDrinks extends Products implements Tax
 {   
+    private String alcoholType; //주류 타입(소주, 맥주)
+    
     /**
      * AlcoholicDrinks 클래스의 객체 생성자
      */
-    public AlcoholicDrinks(String barcode, String name, int price)
+    public AlcoholicDrinks(String barcode, String name, int price, String alcoholType)
     {
         super(barcode, name, price);
+        this.alcoholType = alcoholType;
     }
 
     /**
@@ -22,8 +25,12 @@ public class AlcoholicDrinks extends Products implements Tax
      */
     public double calculateLiquorTax(int price)
     {
-        double originalPrice = price / 2.032;
-        return originalPrice * 0.72;
+        if(alcoholType == "맥주"){
+            return 885.7 * 0.5;
+        }
+        else{
+            return getOriginalPrice() * 0.72;
+        }
     }
 
     /**
@@ -33,19 +40,17 @@ public class AlcoholicDrinks extends Products implements Tax
      */
     public double calculateEduTax(int price)
     {
-        double originalPrice = price / 2.032;
-        return originalPrice * 0.72 * 0.3;
+        return calculateLiquorTax(price) * 0.3;
     }
 
     /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
+     * 부가가치세 계산 메소드
      * @param  price  (세금 포함)금액
      * @return    부가가치세
      */
     public double calculateVAT(int price)
     {
-        double originalPrice = price / 2.032;
-        return(originalPrice + calculateLiquorTax(price) + calculateEduTax(price)) * 0.1;
+        return (getOriginalPrice() + calculateLiquorTax(price) + calculateEduTax(price)) * 0.1;
     }
 
     /**
@@ -53,8 +58,25 @@ public class AlcoholicDrinks extends Products implements Tax
      * @param  quantity  수량
      * @return    최종 금액
      */
+    public int calculatePrice(int quantity)
+    {
+        return getPrice() * quantity;
+    }
+
+    /**
+     * 원가 구하는 메소드
+     * @return    원가
+     */
     public double getOriginalPrice()
     {
-        return getPrice() / 2.032;
+        if(alcoholType == "맥주"){
+            double liquorTax = 885.7 * 0.5;
+            double eduTax = liquorTax * 0.3;
+            double originalPrice = (getPrice() - liquorTax - eduTax) / 1.1;
+            return originalPrice;
+        }
+        else{
+            return getPrice() / 2.1296;
+        }
     }
 }
